7c8895a305cc057ddbf6174d582b96378b2813fb
KakushaVenecia <kakushavenecia@gmail.com>
2022-03-20 23:09:46 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,90 @@
+ $('#order').click(function(event) {
+   event.preventDefault;
+   $('#show').show();
+ });
+
+ function Pizza(type, size,topy,crust,quantity) {
+  this.pizzaType= type;
+  this.pizzaSize = size;
+  this.pizzaTopy= topy;
+  this.pizzaQuantity=quantity;
+  this.pizzaCrust=crust;
+}
+ 
+// $(document).ready(function() {
+//   $("#add-order").click(function(){
+//     $('#add-order').append(
+//       new pizzaType,
+//       new pizzaSize,
+//       new pizzaCrust,
+//       new pizzaTopy,
+//       new pizzaQuantity, 
+
+//       );
+    
+// function resetFields() {
+//         $("input#new-first-name").val("");
+//         $("input#new-last-name").val("");
+//         $("input.new-street").val("");
+//         $("input.new-city").val("");
+//         $("input.new-county").val("");
+//     }
+
+
+
+
+{/* var type = $('.type').on('click', function(event) {
+//   console.log(event.target.id)
+//   return type
+ 
+// });
+//  $('.size').on('click', function(event) {
+//    console.log(event.target.id)
+
+// });
+
+// $('.topy').on('click', function(event) {
+//   console.log(event.target.id)
+  
+// });
+// $('.crust').on('click', function(event) {
+//   console.log(event.target.id)
+   
+// });
+// $('#add').on('click'), function(event){
+//   event.preventDefault();
+// console.log(crust.id, top.id, type.id, size.id)
+// } */}
+
+
+
+
+
+// // let size =document.getElementsByClassName("size")[0].value;
+// // let type =document.getElementsByClassName("type")[0].value;
+// // let topy  =document.getElementsByClassName("topy")[0].value;
+// // let quantity =document.getElementsByClassName("num")[0].value;
+// // let crust =document.getElementsByClassName("crust")[0].value;
+
+// // var addOrder ={size,type,topy,quantity,crust};
+
+
+// // $('#add').click(function (event) {
+// //   event.preventDefault();
+// //   console.log(addOrder);
+// // });
+  
+
+
+
+
+
+  
+   
+
+// // var smallPizza= getElementById("small")
+// // var mediumPizza= getElementById("medium")
+// // var largePizza= getElementById("medium")
+// // var size =["small","medium","large"]
+// // var type=["Periperi","bbq","haw","veg","pep"]
+// // var crust=["crusty","stuffed","gluten"]
